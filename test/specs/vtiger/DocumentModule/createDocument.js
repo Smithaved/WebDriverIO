4d6ab05d25876dbeda53ccccce64d2f8021c9986
Smithaved <smitha20130@gmail.com>
2022-06-26 23:28:06 +0530

    Pom Update

--- test/specs/vtiger/DocumentModule/createDocument.js
+++ test/specs/vtiger/DocumentModule/createDocument.js
@@ -1,23 +1,16 @@
+const LoginPage=require("../../../pageobjects/login.page")
+const HomePage=require("../../../pageobjects/home.page")
 describe("Documents",async ()=>{ 
     it("CreateDocument",async ()=>{
         // launching the application
-        await browser.url("http://localhost:8888/index.php")
+        await LoginPage.open()
         //maximizing the browser
         await browser.maximizeWindow()
-        //featching the title
-        console.log(await browser.getTitle());
         //checking the title is matching
         await expect(browser).toHaveTitleContaining('vtiger CRM 5')
          //login to application by passing login credentials
-         var username=await browser.$('//input[@name="user_name"]')
-         await username.setValue('admin')
-         await expect(username).toHaveValueContaining('admin')
-         var password=await browser.$('//input[@name="user_password"]')
-         await password.setValue('admin')
-         var submit=await browser.$('//input[@id="submitButton"]')
-         await submit.click()
+        await LoginPage.login("admin","admin")
          //check whether home page is displayed or not
-         console.log(await browser.getTitle());
          await expect(browser).toHaveTitleContaining('Home')
         //Create Document
         var document=await browser.$('//a[.="Documents"]')
@@ -35,8 +28,6 @@ describe("Documents",async ()=>{
         var saveOrganization=await browser.$$('//input[@title="Save [Alt+S]"]')
         await saveOrganization[0].click()
          //Mouse hover on administration and signout
-         var element=browser.$('//img[@src="themes/softed/images/user.PNG"]')
-         await element.moveTo()
-         await browser.$('//a[.="Sign Out"]').click()
+         await HomePage.logout()
         })
 })
